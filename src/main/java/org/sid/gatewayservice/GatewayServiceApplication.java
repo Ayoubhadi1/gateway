2195453ccef037ab43ac30842a121dd049574d38
ayoub hadi <ayoub.hadi.stage@worldaudit.ma>
2022-06-24 11:02:21 +0000

    first commit

--- src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
@@ -16,15 +16,17 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
-   /* @Bean
+    /*@Bean
     RouteLocator routeLocator(RouteLocatorBuilder builder){
         //si on connait où se trouve les MS
         return builder.routes()
                 .route((r)->r.path("/customers/**").uri("http://localhost:8081"))
                 .route((r)->r.path("/products/**").uri("http://localhost:8082"))
+                .route((r)->r.path("/login" , "/auth/**","/users/**","/users").uri("http://localhost:8089"))
+                .route((r)->r.path("/m1/**").uri("http://localhost:8095"))
                 .build();
 
-        //si je connait les noms des MS
+        si je connait les noms des MS
         return builder.routes()
                 .route((r)->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))   //à chaque fois que tu reçois une requete qui contient /customers/** envoie cette requete à une service qui s'appel customer-service , je suis pas obligé de connaitre la machine où se trouve
                 .route((r)->r.path("/products/**").uri("lb://PRODUCT-SERVICE"))
